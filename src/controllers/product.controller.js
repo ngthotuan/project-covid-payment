9e0bbc349e8ff8b07149fc268298c880de8d729d
ngthotuan <ngthotuan@gmail.com>
2021-12-23 15:35:25 +0700

    create simple product + show product list

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,12 +1,22 @@
-function index(req, res, next) {
-    res.render('products/list', { title: 'Danh sách sản phẩm' });
-}
+const { productService } = require('../services');
 
-function newProduct(req, res, next) {
+const index = async (req, res, next) => {
+    const products = await productService.findAll();
+    res.render('products/list', { title: 'Danh sách sản phẩm', products });
+};
+
+const getCreate = (req, res, next) => {
     res.render('products/form', { title: 'Thêm sản phẩm' });
-}
+};
+
+const postCreate = async (req, res, next) => {
+    console.log(req.body);
+    await productService.create(req.body);
+    res.redirect('/products');
+};
 
 module.exports = {
     index,
-    newProduct,
+    getCreate,
+    postCreate,
 };
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { productController } = require('../controllers');
 
 router.get('/', productController.index);
-router.get('/new', productController.newProduct);
+router.get('/create', productController.getCreate);
+router.post('/create', productController.postCreate);
 
 module.exports = router;
