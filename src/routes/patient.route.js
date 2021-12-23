e253274463e848fa4e2855e3cf17f95541c6d7eb
tuan2012 <phampham1109@gmail.com>
2021-12-23 23:06:56 +0700

    update patient

--- src/routes/patient.route.js
+++ src/routes/patient.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { accountController } = require('../controllers');
+const { patientController } = require('../controllers');
 
-router.get('/', accountController.list);
+router.get('/', patientController.getList);
+router.get('/create', patientController.getCreate);
+router.post('/create', patientController.postCreate);
+router.get('/', patientController.getDetail);
 
 module.exports = router;
