eda5b475d90b8cb0b389b3c726abdcfaabd00654
ngthotuan <ngthotuan@gmail.com>
2021-12-28 16:15:14 +0700

    remove unused files

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const siteRouter = require('./site');
-// const testRouter = require('./test');
-
-function route(app) {
-  app.use('/', siteRouter);
-  // app.use('/test', testRouter);
-}
-
-module.exports = route;
