2e5c97523a974ad90b06eedba540aa3c7f1f590a
Minhvu17ctt7 <Minhvu01292918027@gmail.com>
2022-01-16 16:48:07 +0700

    user categories

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,34 @@
-const { accountService } = require('../services');
+const {
+    accountService,
+    categoryService,
+    productService,
+} = require('../services');
+
+const {} = require('../services');
+
+const getList = async (req, res, next) => {
+    const categories = await categoryService.findAll();
+    res.render('categories/list', {
+        title: 'Danh sách gói',
+        role: 'USER',
+        categories,
+    });
+};
+
+const detail = async (req, res, next) => {
+    const id = req.params.id;
+    const category = await categoryService.findCategoryIncludeProduct(id);
+    res.render('categories/view', {
+        title: 'Chi tiết gói',
+        role: 'USER',
+        category,
+    });
+};
 
 const index = async (req, res, next) => {};
 
 module.exports = {
     index,
+    getList,
+    detail,
 };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const { userController } = require('../controllers');
 
+router.get('/categories', userController.getList);
+router.get('/categories/:id', userController.detail);
 router.get('/', userController.index);
 
 module.exports = router;
