634e2e3a10718648ba104888170c0026cb0b7b84
tuannt7 <tuannt7@vng.com.vn>
2022-06-18 18:26:33 +0700

    update db connect

--- src/db/index.js
+++ src/db/index.js
@@ -1,22 +1,31 @@
 const { Sequelize } = require('sequelize');
-const models = require('../models');
 require('dotenv').config();
+const models = require('../models');
 
 // Option 3: Passing parameters separately (other dialects)
-const { DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_DIALECT } = process.env;
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-    host: DB_HOST,
-    dialect: DB_DIALECT /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
-    logging: false,
-});
+const { DB_URL, DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_DIALECT } = process.env;
+const sequelize = DB_URL
+    ? new Sequelize(DB_URL, {
+          dialect: DB_DIALECT,
+          logging: false,
+          dialectOptions: { ssl: { rejectUnauthorized: false } },
+      })
+    : new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+          host: DB_HOST,
+          dialect:
+              DB_DIALECT /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
+          logging: false,
+          dialectOptions: { ssl: { rejectUnauthorized: false } },
+      });
 
 async function connect() {
     try {
         await sequelize.authenticate();
         const allModels = models(sequelize);
         await Promise.all(
-            Object.values(allModels).map((model) =>
-                model.sync({ alter: true }),
+            Object.values(allModels).map(
+                (model) => model.sync(),
+                // (model) => model.sync({ alter: true })
             ),
         );
         console.log('Connection has been established successfully.');
